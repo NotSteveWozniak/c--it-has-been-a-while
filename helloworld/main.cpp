1f2d757ff523d1bbce92ecc0ec7343e0e40cbe01
New User <newuser@craymini.mediacom.info>
2021-08-21 11:52:55 -0500

    Initial Commit

---
+++ helloworld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  helloworld
+//
+//  Created by New User on 8/21/21.
+//  Copyright © 2021 New User. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
